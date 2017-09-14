782dcdcb29ff1d976538bfefde7aa0aefbf39657
Valeryi <baibossynov.valery@gmail.com>
2017-09-14 00:59:57 +0000

    Litle typo fix

--- src/test/scala/domain/TokenValueSpec.scala
+++ src/test/scala/domain/TokenValueSpec.scala
@@ -8,7 +8,7 @@ class TokenValueSpec extends WordSpec with Matchers with MockitoSugar {
   val testValue = "1969406143212345|H6FF85UiGASDFdB1_Zviht7lzxc"
 
   "TokenValue#fromRaw" should {
-    s"creates TokenValue from raw value in format = $testValue" in {
+    s"create TokenValue from raw value in format = $testValue" in {
       TokenValue.fromRaw(testValue) shouldBe TokenValue("H6FF85UiGASDFdB1_Zviht7lzxc")
     }
   }
