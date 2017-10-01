0b8971e6dcfffb47f0f6433081b7fcc6df946995
Valeryi <baibossynov.valery@gmail.com>
2017-10-01 18:12:59 +0000

    Error test case for userAccessToken

--- src/test/scala/api/UserAccessTokenSpec.scala
+++ src/test/scala/api/UserAccessTokenSpec.scala
@@ -1,9 +1,7 @@
 package api
 
-import java.time.Instant
-
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
 import akka.stream.ActorMaterializer
 import config.FacebookConfig._
 import domain.UserAccessToken
@@ -19,9 +17,39 @@ import scala.concurrent.duration.DurationInt
 import scala.io.Source
 
 class UserAccessTokenSpec
-  extends AsyncWordSpec with Matchers with MockitoSugar with MockedAsyncRequestService {
+  extends AsyncWordSpec with Matchers with MockitoSugar {
 
   trait ClientProbe extends FacebookInternals {
+    val facebookServices = mock[FacebookInternals]
+    val mockAsyncRequestService = mock[AsyncRequestService]
+
+
+
+    def mockSendWithResource(resourcePath: String) = {
+      when(mockAsyncRequestService.sendRequest(anyObject[URLBuilder])).thenReturn(
+        Future.successful(
+          HttpResponse(
+            entity = HttpEntity(
+              contentType = ContentTypes.`application/json`,
+              string      = Source.fromResource(resourcePath).mkString)
+          )
+        )
+      )
+    }
+
+    def mockSendError(resourcePath: String) = {
+      when(mockAsyncRequestService.sendRequest(anyObject[URLBuilder])).thenReturn(
+        Future.successful(
+          HttpResponse(
+            status = StatusCodes.BadRequest,
+            entity = HttpEntity(
+              contentType = ContentTypes.`application/json`,
+              string      = Source.fromResource(resourcePath).mkString)
+          )
+        )
+      )
+    }
+
     override implicit lazy val system = ActorSystem()
     override implicit lazy val mat = ActorMaterializer()
     override implicit lazy val ec = system.dispatcher
@@ -29,10 +57,12 @@ class UserAccessTokenSpec
     override val transformer = new DomainTransformer()
   }
 
+
   "Facebook Graph Api" should {
     "return user access token" in {
-      mockSendWithResource(resourcePath = "testdata/user_access_token.json")
       val client = new FacebookClient(clientId, appSecret) with ClientProbe
+
+      client.mockSendWithResource(resourcePath = "testdata/user_access_token.json")
       client.userAccessTokenEither("code") map {
         case Right(token) =>
           token.tokenValue.value shouldBe "test token"
@@ -40,28 +70,16 @@ class UserAccessTokenSpec
          case Left(e) => fail(e.error.message)
       }
     }
+
+    "return error in wrong code" in {
+      val client = new FacebookClient(clientId, appSecret) with ClientProbe
+
+      client.mockSendError(resourcePath = "testdata/user_access_token_wrong_code.json")
+      client.userAccessTokenEither("code") map {
+        case Right(token) => fail("left expected")
+        case Left(e) => e.error.message shouldBe "Invalid verification code format."
+      }
+    }
   }
 
-}
-
-trait MockedAsyncRequestService extends MockitoSugar {
-  implicit val actorSystem = ActorSystem()
-  val materializer = ActorMaterializer()
-
-  val facebookServices = mock[FacebookInternals]
-  val mockAsyncRequestService = mock[AsyncRequestService]
-
-
-
-  def mockSendWithResource(resourcePath: String) = {
-    when(mockAsyncRequestService.sendRequest(anyObject[URLBuilder])).thenReturn(
-      Future.successful(
-        HttpResponse(
-          entity = HttpEntity(
-            contentType = ContentTypes.`application/json`,
-            string      = Source.fromResource(resourcePath).mkString)
-        )
-      )
-    )
-  }
 }
