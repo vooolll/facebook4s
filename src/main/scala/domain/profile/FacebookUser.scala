e174856a7ea6b3206d5349eba57d2f4a006a62b3
valeriybaibossynov <vbaibossynov@evolutiongaming.com>
2017-11-25 13:18:40 +0200

    User picture

--- src/main/scala/domain/profile/FacebookUser.scala
+++ src/main/scala/domain/profile/FacebookUser.scala
@@ -8,5 +8,21 @@ final case class FacebookUserId(value: String)
 /**
   * @param id facebook user id - numeric string
   * @param name facebook user first name
+  * @param picture optional picture
   */
-case class FacebookUser(id: FacebookUserId, name: String) extends FacebookProfile
+case class FacebookUser(
+  id      : FacebookUserId,
+  name    : String,
+  picture : Option[FacebookUserPicture]) extends FacebookProfile
+
+/**
+  * @param height image height
+  * @param isSilhouette is silhouette
+  * @param url link
+  * @param wight image wight
+  */
+case class FacebookUserPicture(
+  height       : Double,
+  isSilhouette : Boolean,
+  url          : String,
+  wight        : Double)
