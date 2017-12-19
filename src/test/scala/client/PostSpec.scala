68d76528939115185d47469a2e164108fb87b6e9
valeriybaibossynov <vbaibossynov@evolutiongaming.com>
2017-12-19 17:47:28 +0200

    facebook4s-42 Add integration tests

--- src/test/scala/client/PostSpec.scala
+++ src/test/scala/client/PostSpec.scala
@@ -8,16 +8,16 @@ import serialization.compatibility.toInstant
 
 class PostSpec extends FacebookClientSupport {
 
-  val realPostId = FacebookPostId("499283963749541_527696260908311")
+  val realPostId = FacebookPostId("117656352360395_120118735447490")
 
   val realPost = FacebookPost(
-    FacebookPostId("499283963749541_527696260908311"),
-    Some("Valeryi Baibossynov updated his cover photo."),
-    Some(toInstant("2017-11-27T14:54:19+0000")),
-    Some("527696177574986"),
-    Some("https://scontent.xx.fbcdn.net/v/t1.0-0/q86/s130x130/23905322_527696177574986_8012137948429389386_n.jpg" +
-      "?oh=077a8c93633c68f1666047708f1fe749&oe=5ACCB40D"),
-    Some(FacebookProfileId("499283963749541")))
+    realPostId,
+    Some("Bob Willins updated her cover photo."),
+    Some(toInstant("2017-12-19T14:08:44+0000")),
+    Some("120118675447496"),
+    Some("https://scontent.xx.fbcdn.net/v/t1.0-0/s130x130/25398995_120118675447496_5830741756468130361_n.jpg?" +
+      "oh=63adaf511c1129c82283d1ddc4244448&oe=5ACE1813"),
+    Some(FacebookProfileId("117656352360395")))
 
   "Facebook Graph Api" should {
     "return posts" in { c =>
