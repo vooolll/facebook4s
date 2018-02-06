ca8ac31fc99832a101a708da8af3d23801f058e8
valeriybaibossynov <vbaibossynov@evolutiongaming.com>
2018-02-06 16:43:30 +0200

    facebook4s-73 Add overloaded methods for photo api

--- src/main/scala/client/FacebookPhotoApi.scala
+++ src/main/scala/client/FacebookPhotoApi.scala
@@ -1,6 +1,6 @@
 package client
 
-import client.FacebookClient.accessToken
+import client.FacebookClient.{AccessToken, accessToken}
 import domain.albums.photo.FacebookPhotoAttributes._
 import domain.albums.photo.{FacebookPhoto, FacebookPhotoId}
 import domain.oauth.FacebookOauthError
@@ -12,23 +12,89 @@ trait FacebookPhotoApi extends FacebookInternals {
 
   import serialization.FacebookDecoders.decodePhoto
 
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessToken User access token
+    * @param fields Sequence of facebook photo attributes
+    * @return future FacebookPhoto
+    */
+  def photo(
+    photoId     : FacebookPhotoId,
+    accessToken : AccessToken,
+    fields      : Seq[FacebookPhotoAttribute]): Future[FacebookPhoto] =
+    sendRequestOrFail(photoUri(photoId, accessToken, fields))
+
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessToken User access token
+    * @return future FacebookPhoto
+    */
+  def photo(
+      photoId     : FacebookPhotoId,
+      accessToken : AccessToken): Future[FacebookPhoto] =
+    photo(photoId, accessToken, defaultPhotoAttributeValues)
+
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessTokenValue String representation of user access token
+    * @param fields Sequence of facebook photo attributes
+    * @return FacebookPhoto
+    */
   def photo(
     photoId          : FacebookPhotoId,
     accessTokenValue : String,
     fields           : Seq[FacebookPhotoAttribute]): Future[FacebookPhoto] =
-    sendRequestOrFail(photoUri(photoId, accessToken(accessTokenValue), fields))
+    photo(photoId, accessToken(accessTokenValue), fields)
 
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessTokenValue String representation of user access token
+    * @return FacebookPhoto
+    */
   def photo(
     photoId          : FacebookPhotoId,
     accessTokenValue : String): Future[FacebookPhoto] =
     photo(photoId, accessTokenValue, defaultPhotoAttributeValues)
 
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessToken User access token
+    * @param fields Sequence of facebook photo attributes
+    * @return future either FacebookPhoto or FacebookOauthError
+    */
+  def photoResult(
+    photoId     : FacebookPhotoId,
+    accessToken : AccessToken,
+    fields      : Seq[FacebookPhotoAttribute]): Future[Either[FacebookOauthError, FacebookPhoto]] =
+    sendRequest(photoUri(photoId, accessToken, fields))
+
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessToken User access token
+    * @return future either FacebookPhoto or FacebookOauthError
+    */
+  def photoResult(
+      photoId     : FacebookPhotoId,
+      accessToken : AccessToken): Future[Either[FacebookOauthError, FacebookPhoto]] =
+    photoResult(photoId, accessToken, defaultPhotoAttributeValues)
+
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessTokenValue String representation of user access token
+    * @param fields Sequence of facebook photo attributes
+    * @return future either FacebookPhoto or FacebookOauthError
+    */
   def photoResult(
     photoId          : FacebookPhotoId,
     accessTokenValue : String,
     fields           : Seq[FacebookPhotoAttribute]): Future[Either[FacebookOauthError, FacebookPhoto]] =
-    sendRequest(photoUri(photoId, accessToken(accessTokenValue), fields))
+    photoResult(photoId, accessToken(accessTokenValue), fields)
 
+  /**
+    * @param photoId Id of facebook photo
+    * @param accessTokenValue String representation of user access token
+    * @return future either FacebookPhoto or FacebookOauthError
+    */
   def photoResult(
     photoId          : FacebookPhotoId,
     accessTokenValue : String): Future[Either[FacebookOauthError, FacebookPhoto]] =
