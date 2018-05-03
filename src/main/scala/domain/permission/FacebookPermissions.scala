07ceda2f91a161a36f45eeaba38dde55dcf13ca4
valeriybaibossynov <vbaibossynov@evolutiongaming.com>
2018-05-03 20:45:39 +0300

    facebook4s-115 Add up to date user permissions

--- src/main/scala/domain/permission/FacebookPermissions.scala
+++ src/main/scala/domain/permission/FacebookPermissions.scala
@@ -11,40 +11,21 @@ object FacebookPermissions {
     val value: String
   }
 
-  /**
-    * Facebook permission that gives access to user posts.
-    */
-  final case object FacebookUserPosts extends FacebookPermission {
-    val value = "user_posts"
-  }
-
-  object UserAttributes {
-    val attributeValues = Seq(Email, PublicProfile, ReadCustomFriendList, AboutMe, BirthDay, EducationHistory,
-      Fiends, Hometown, Location, RelationshipDetails, UserRelationship, ReligionPolitics)
+  object UserDataPermissions {
+    val values = Seq(Email, AgeRange, BirthDay, Fiends, Hometown, Gender, Likes, Location, Photos, Videos,
+      Status, TaggedPlaces, Posts)
 
 
     final case object Email extends FacebookPermission {
       override val value = "email"
     }
 
-    final case object PublicProfile extends FacebookPermission {
-      override val value = "public_profile"
-    }
-
-    final case object ReadCustomFriendList extends FacebookPermission {
-      override val value = "read_custom_friendlists"
-    }
-
-    final case object AboutMe extends FacebookPermission {
-      override val value = "user_about_me"
+    final case object AgeRange extends FacebookPermission {
+      override val value = "user_age_range"
     }
 
     final case object BirthDay extends FacebookPermission {
       override val value = "user_birthday"
-    }
-
-    final case object EducationHistory extends FacebookPermission {
-      override val value = "user_education_history"
     }
 
     final case object Fiends extends FacebookPermission {
@@ -55,28 +36,52 @@ object FacebookPermissions {
       override val value = "user_hometown"
     }
 
+    final case object Gender extends FacebookPermission {
+      override val value = "user_gender"
+    }
+
+    final case object Likes extends FacebookPermission {
+      override val value = "user_likes"
+    }
+
     final case object Location extends FacebookPermission {
       override val value = "user_location"
     }
 
-    final case object RelationshipDetails extends FacebookPermission {
-      override val value = "user_relationship_details"
-    }
-
-    final case object UserRelationship extends FacebookPermission {
-      override val value = "user_relationships"
-    }
-
-    final case object ReligionPolitics extends FacebookPermission {
-      override val value = "user_religion_politics"
-    }
-
-    final case object UserPhotos extends FacebookPermission {
+    final case object Photos extends FacebookPermission {
       override val value = "user_photos"
     }
 
-    final case object UserVideos extends FacebookPermission {
+    final case object Videos extends FacebookPermission {
       override val value = "user_videos"
+    }
+
+    final case object Status extends FacebookPermission {
+      override val value = "user_status"
+    }
+
+    final case object TaggedPlaces extends FacebookPermission {
+      override val value = "user_tagged_places"
+    }
+
+    final case object Posts extends FacebookPermission {
+      val value = "user_posts"
+    }
+  }
+
+  object EventsGroupsPages {
+    val values = Seq(ManagePages, PagesShowList, PublishPages)
+
+    final case object ManagePages extends FacebookPermission {
+      override val value = "manage_pages"
+    }
+
+    final case object PagesShowList extends FacebookPermission {
+      override val value = "pages_show_list"
+    }
+
+    final case object PublishPages extends FacebookPermission {
+      override val value = "publish_pages"
     }
   }
 
